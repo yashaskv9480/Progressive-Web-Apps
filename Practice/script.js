1cc374a433eaa70bcb2cdf1dafe3c67ac81bc798
Yashas K V <yashas9480@gmail.com>
2024-02-16 10:41:43 +0530

    Feb 16th

--- Practice/script.js
+++ Practice/script.js
@@ -1,1 +1,33 @@
-console.log("Hello from nodejs")
+console.log("Your viewing the content inside the console log")
+
+let firstName = "Yashas"
+let lastName = "K V"
+
+console.log(`${firstName, lastName} `)
+
+const ctc = 6.72;
+
+//ctc = 10;
+
+console.log(ctc)
+
+//static 
+
+//int age =10;
+
+//String name = "Yashas"
+
+//dynamic
+
+let age = 10
+
+console.log(age + " " + firstName)
+
+//objects
+
+let students = {
+    "usn" : "1RV22MC114",
+    "name": "Yashas"
+}
+
+console.log(students)
